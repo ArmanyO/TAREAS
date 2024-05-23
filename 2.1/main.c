da0376e507bad41f659f68c225523e6e3cf1715f
Armany <armanyemaorte@outloock.com>
2024-05-23 04:43:25 -0400

    programas del capitulo 2

---
+++ 2.1/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+
+void main(void)
+{
+    float PRO;
+    printf("ingrese el promedio del alumno: ");
+    scanf("%f", &PRO);
+    if (PRO >= 6)
+
+        printf("\nAprobado");
+
+    if ( PRO <=5)
+
+    printf("\nReprobado");
+
+}
